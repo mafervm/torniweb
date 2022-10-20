660d2e68e4185d7cb7d32d76c15ac16d0f8b0ebf
mafervm <mariafernandavilla21@live.com.mx>
2022-10-19 23:43:42 -0500

    Actualizacion5

--- app/cotizador.js
+++ app/cotizador.js
@@ -3,7 +3,7 @@ const cartContainer = document.querySelector('.cart-container')
 const cotizadorBtn = document.querySelector('.cotizadorBtn')
 const confirmModal = document.querySelector('#form-modal')
 const formContact = document.querySelector('#contact-form')
-const BASE_URL = 'https://josue.ngrok.io/api/client'
+const BASE_URL = 'http://127.0.0.1:3333/api/client'
 const S3_URL = 'https://torniweb.s3.amazonaws.com/'
 const ERROR_CODES = {
   '1': 'Ingresa un número válido',
@@ -134,11 +134,14 @@ function closeModal() {
 }
 
 function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  var id_token = googleUser.getAuthResponse().id_token;
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://josue.ngrok.io');
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhr.onload = function() {
+  console.log('Signed in as: ' + xhr.responseText);
+  };
+  xhr.send('idtoken=' + id_token);
 }
 
 
